07d13794ce2b55cfcca7a3dc4a30b5a3e8241bb6
Yukun_Lee <214503210@qq.com>
2018-09-17 15:04:49 +0800

    update

--- test/src/t16/T16_Solution.java
+++ test/src/t16/T16_Solution.java
@@ -1,34 +1,32 @@
 package t16;
 
-import java.math.BigDecimal;
-
 public class T16_Solution {
-	public int maxPoints(Point[] point) {
+	public int maxPoints(Point[] points) {
 		// BigDecimal k;
 		// BigDecimal b;
 		double k1;
 		int b1;
 		int vertical = 0;
 		int maxNum = 0;
-		if (point.length == 0) {
+		if (points.length == 0) {
 			return 0;
 		}
-		if (point.length == 1) {
+		if (points.length == 1) {
 			return 1;
 		}
-		for (int i = 0; i < point.length; i++) {
-			for (int j = i + 1; j < point.length; j++) {
+		for (int i = 0; i < points.length; i++) {
+			for (int j = i + 1; j < points.length; j++) {
 				int count = 0;
-				if (point[i].x == point[j].x) {
-					vertical = point[i].x;
-					for (int n = 0; n < point.length; n++) {
-						if (point[n].x == vertical) {
+				if (points[i].x == points[j].x) {
+					vertical = points[i].x;
+					for (int n = 0; n < points.length; n++) {
+						if (points[n].x == vertical) {
 							count++;
 						}
 					}
 
 				} else {
-					/*
+	/*				
 					 * 
 					 * BigDecimal a1 = new BigDecimal(point[i].y); BigDecimal a2 = new
 					 * BigDecimal(point[i].x); BigDecimal b1 = new BigDecimal(point[j].y);
@@ -41,22 +39,27 @@ public class T16_Solution {
 					 * BigDecimal(point[n].y); BigDecimal c2 = new BigDecimal(point[n].x); if
 					 * (c1.equals(c2.multiply(k).add(b).setScale(0,
 					 * java.math.BigDecimal.ROUND_HALF_UP)) ) { count++; } }
-					 */
-
+					 
+*/
+					
+					  k1 = (points[i].y - points[j].y) / (points[i].x - points[j].x); 
+					  b1 = (int)(points[i].y - k1 * points[i].x);
+					  
+					  for (int n = 0; n < points.length; n++) { 
+						  if (Math.round(points[n].y) ==Math.round((int)(points[n].x *k1) + b1) ) { 
+						  count++;
+						  }
+					  }
+					 
+					
 					/*
-					 * k1 = (point[i].y - point[j].y) / (point[i].x - point[j].x); 
-					 * b1 = (int)(point[i].y - k1 * point[i].x);
-					 * 
-					 * for (int n = 0; n < point.length; n++) { if (point[n].y == (int)(point[n].x *
-					 * k1) + b1) { count++; } }
-					 */
-
 					for (int n = 0; n < point.length; n++) {
 						if (point[n].y == (int) (point[n].x * (point[i].y - point[j].y) / (point[i].x - point[j].x))
 								+ (int)(point[i].y - (point[i].y - point[j].y) / (point[i].x - point[j].x) * point[i].x)) {
 							count++;
 						}
 					}
+					*/
 				}
 				maxNum = Math.max(maxNum, count);
 			}
