07c98d2bd4ec4ce7ae52179b82e7a25251c3ed53
Yukun_Lee <214503210@qq.com>
2018-09-28 18:52:42 +0800

    update

---
+++ test/src/t23/T23_test.java
@@ -0,0 +1,15 @@
+package t23;
+
+public class T23_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "abcde";
+		String str1 = "ab";
+		
+		str = str.substring(str.indexOf(str1));
+		System.out.println(str);
+
+	}
+
+}
