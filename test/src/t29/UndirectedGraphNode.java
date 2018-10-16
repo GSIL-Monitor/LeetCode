f05283adee5ac1d98cf4a451a5505f302d8c67b5
Yukun_Lee <214503210@qq.com>
2018-10-16 16:33:49 +0800

    update

--- test/src/t29/UndirectedGraphNode.java
+++ test/src/t29/UndirectedGraphNode.java
@@ -3,7 +3,11 @@ package t29;
 import java.util.ArrayList;
 
 public class UndirectedGraphNode {
-		  int label;
-		  ArrayList<UndirectedGraphNode> neighbors;
-		  UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }	 
+	int label;
+	ArrayList<UndirectedGraphNode> neighbors;
+
+	UndirectedGraphNode(int x) {
+		label = x;
+		neighbors = new ArrayList<UndirectedGraphNode>();
+	}
 }
