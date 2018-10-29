e831bdd09aa899dcfa00f25227f236fa268a2bfc
Yukun_Lee <214503210@qq.com>
2018-10-29 09:38:29 +0800

    update

--- test/src/t36/T36_Solution.java
+++ test/src/t36/T36_Solution.java
@@ -1,6 +1,5 @@
 package t36;
 
-
 public class T36_Solution {
 	/**
 	 * @author yukunlee
@@ -9,16 +8,16 @@ public class T36_Solution {
 	 */
 	public int maxProfit3(int[] prices) {
 		int len = prices.length;
-		int buy1 = Integer.MIN_VALUE,sell1= 0, buy2= Integer.MIN_VALUE, sell2 = 0;
-		for(int i = 0 ; i < len ;++i) {
+		int buy1 = Integer.MIN_VALUE, sell1 = 0, buy2 = Integer.MIN_VALUE, sell2 = 0;
+		for (int i = 0; i < len; ++i) {
 			buy1 = Math.max(buy1, -prices[i]);
 			sell1 = Math.max(sell1, prices[i] + buy1);
-			buy2 = Math.max(buy2, sell1-prices[i]);
-			sell2 = Math.max(sell2, prices[i]+buy2);
+			buy2 = Math.max(buy2, sell1 - prices[i]);
+			sell2 = Math.max(sell2, prices[i] + buy2);
 		}
 		return sell2;
 	}
-	
+
 	/**
 	 * @author yukunlee
 	 * @Description best-time-to-buy-and-sell-stock-ii
@@ -26,14 +25,40 @@ public class T36_Solution {
 	 * @param prices
 	 * @return
 	 */
-	public int maxProfit1(int[] prices) {
-		int buy = Integer.MIN_VALUE, sell = 0;
+	public int maxProfit2(int[] prices) {
+		int profit = 0;
 		int len = prices.length;
-		for(int i = 0 ; i < len ; ++i) {
-			buy = Math.max(buy, -prices[i]);
-			sell = Math.max(buy, sell + buy);
+		int diff = 0;
+		if (len < 2) {
+			return 0;
 		}
-		return sell;	
+		for (int i = 0; i < len - 1; ++i) {
+			diff = prices[i + 1] - prices[i];
+			if (diff > 0) {
+				profit += diff;
+			}
+		}
+		return profit;
 	}
-	
+
+	/**
+	 * @author yukunlee
+	 * @Description best-time-to-buy-and-sell-stock-i
+	 * @date 2018年10月29日
+	 * @param prices
+	 * @return
+	 */
+	public int maxProfit1(int[] prices) {
+		int len = prices.length;
+		if (len < 2) {
+			return 0;
+		}
+		int buy = Integer.MAX_VALUE, sell = 0;
+		for (int i = 0; i < len; ++i) {
+			buy = Math.min(buy, prices[i]);
+			sell = Math.max(sell, prices[i] - buy);
+		}
+		return sell;
+	}
+
 }
