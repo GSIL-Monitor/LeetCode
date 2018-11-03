89d14f965da170f269f2a2c5515991645806d1db
Yukun_Lee <214503210@qq.com>
2018-11-03 20:21:47 +0800

    update

--- test/src/t45/T45_Solution.java
+++ test/src/t45/T45_Solution.java
@@ -3,6 +3,14 @@ package t45;
 import t14.TreeNode;
 
 public class T45_Solution {
+    /**
+     * @author yukunlee
+     * @Description construct-binary-tree-from-preorder-and-inorder-traversal
+     * @date 2018年11月3日
+     * @param preorder
+     * @param inorder
+     * @return
+     */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
     	if(preorder.length == 0) {
     		return null;
@@ -21,6 +29,36 @@ public class T45_Solution {
     	if(mid != head) root.left = recursion(preorder, inorder, head,mid-1, first+1);
     	first = first + mid - head;
     	if(mid != tail) root.right = recursion(preorder, inorder, mid+1,tail, first+1);  	
+    	return root;
+    }
+    
+    /**
+     * @author yukunlee
+     * @Description construct-binary-tree-from-inorder-and-postorder-traversal
+     * @date 2018年11月3日
+     * @param inorder
+     * @param postorder
+     * @return
+     */
+    public TreeNode buildTree_1(int[] inorder, int[] postorder) {
+    	if(postorder.length == 0) {
+    		return null;
+    	}
+    	return recursion_1(postorder, inorder, 0, inorder.length-1, inorder.length-1);
+    }
+    
+    private TreeNode recursion_1(int[] postorder, int[] inorder, int head,int tail, int last) {
+    	TreeNode root = new TreeNode(postorder[last]);
+    	int mid = -1;
+    	for(int i = tail ; i >= head; --i) {  
+    		if(inorder[i] == postorder[last]) {
+    			mid = i;
+    			break;
+    		}
+    	}
+    	if(mid != tail) root.right = recursion_1(postorder, inorder, mid+1,tail, last-1);  	
+    	last = last -tail + mid;
+    	if(mid != head) root.left = recursion_1(postorder, inorder, head,mid-1, last-1);
     	return root;
     }
 
--- test/src/t45/T45_Test.java
+++ test/src/t45/T45_Test.java
@@ -1,13 +1,16 @@
 package t45;
 
+import t14.TreeNode;
+
 public class T45_Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {1,2,3};
-		int[] b = {2,3,1};
+		int[] a = {1,2,3,4};
+		int[] b = {1,3,4,2};
 		T45_Solution t = new T45_Solution();
-		t.buildTree(a, b);
+		TreeNode n = t.buildTree_1(a, b);
+		System.out.println(n.val +"+"+n.left.val+"+"+n.right.val);
 
 	}
 
