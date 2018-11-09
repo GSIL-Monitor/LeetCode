e31b58320032ee4502fe127514641b663800a958
Yukun_Lee <214503210@qq.com>
2018-11-09 15:47:09 +0800

    update

--- test/src/t46/T46_Solution.java
+++ test/src/t46/T46_Solution.java
@@ -91,35 +91,36 @@ public class T46_Solution {
 				if (node.left != null) {
 					queue.add(node.left);
 					arr.add(node.left.val);
-				}else {
+				} else {
 					arr.add(0);
 				}
 				if (node.right != null) {
 					queue.add(node.right);
 					arr.add(node.right.val);
-				}else {
+				} else {
 					arr.add(0);
 				}
 				--size;
 			}
-			if(!isHalfEqual(arr)) return false;
+			if (!isHalfEqual(arr))
+				return false;
 		}
 		return true;
 	}
-	
+
 	private boolean isHalfEqual(ArrayList<Integer> arr) {
-		int len = arr.size()-1;
-		if(len <= 0) {
+		int len = arr.size() - 1;
+		if (len <= 0) {
 			return true;
 		}
-		for(int i = 0 ; i <= len/2 ; ++i) {
-			if(arr.get(i) != arr.get(len-i)) {
+		for (int i = 0; i <= len / 2; ++i) {
+			if (arr.get(i) != arr.get(len - i)) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	/**
 	 * @author yukunlee
 	 * @Description symmetric-tree recursion
@@ -127,43 +128,107 @@ public class T46_Solution {
 	 * @param root
 	 * @return
 	 */
-	public boolean isSymmetric_recursion(TreeNode root) {		
+	public boolean isSymmetric_recursion(TreeNode root) {
 		return isEqual(root, root);
 	}
+
 	private boolean isEqual(TreeNode left, TreeNode right) {
-		if(left == null && right == null) return true;
-		if((left == null && right != null)||left != null && right == null) return false;
-		return left.val == right.val && isEqual(left.left , right.right) && isEqual(left.right , right.left);
+		if (left == null && right == null)
+			return true;
+		if ((left == null && right != null) || left != null && right == null)
+			return false;
+		return left.val == right.val && isEqual(left.left, right.right) && isEqual(left.right, right.left);
+	}
+
+	/**
+	 * @author yukunlee
+	 * @Description isSameTree
+	 * @date 2018年11月9日
+	 * @param p
+	 * @param q
+	 * @return
+	 */
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if (p == null && q == null) {
+			return true;
+		} else if (p == null || q == null) {
+			return false;
+		} else if (p.val != q.val) {
+			return false;
+		}
+		return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+	}
+
+	/**
+	 * @author yukunlee
+	 * @Description recover-binary-search-tree
+	 * @date 2018年11月9日
+	 * @param root
+	 */
+	public void recoverTree(TreeNode root) {
+		TreeNode cur = root, pre = null;
+		while (cur != null) {
+			if (cur.left == null) {
+				detect(pre, cur);
+				pre = cur;
+				cur = cur.right;
+			} else {
+				TreeNode node = cur.left;
+				while (node.right != null && node.right != cur) {
+					node = node.right;
+				}
+				if (node.right == null) {
+					node.right = cur;
+					cur = cur.left;
+				} else {
+					detect(pre, cur);
+					node.right = null;
+					pre = cur;
+					cur = cur.right;
+				}
+			}
+		}
+	}
+
+	private void detect(TreeNode pre, TreeNode cur) {
+		if (pre != null && cur != null) {
+			if (pre.val > cur.val) {
+				int n = pre.val;
+				pre.val = cur.val;
+				cur.val = n;
+			}
+		}
+	}
+
+	/**
+	 * @author yukunlee
+	 * @Description TODO
+	 * @date 2018年11月9日
+	 * @param root
+	 */
+	TreeNode firstElem = null, secondElem = null;
+	TreeNode preElem = new TreeNode(Integer.MIN_VALUE);
+	
+	public void recoverTree_recursion(TreeNode root) {
+		// 中序遍历
+		inOrderTraverse(root);
+		int tmp = firstElem.val;
+		firstElem.val = secondElem.val;
+		secondElem.val = tmp;
+	}
+	private void inOrderTraverse(TreeNode root) {
+		if (root == null)
+			return;
+		inOrderTraverse(root.left);
+		if (firstElem == null && preElem.val >= root.val) {
+			firstElem = preElem;
+		}
+		if (firstElem != null && preElem.val >= root.val) {
+			secondElem = root;
+		}
+		preElem = root;
+		inOrderTraverse(root.right);
 	}
 	
-    /**
-     * @author yukunlee
-     * @Description isSameTree
-     * @date 2018年11月9日
-     * @param p
-     * @param q
-     * @return
-     */
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) {
-        	return true;
-        }else if(p == null ||q == null){
-        	return false;
-        }else if(p.val != q.val ) {
-        	return false;
-        }
-    	return isSameTree(p.left, q.left)&&isSameTree(p.right, q.right);
-    }
-    
-    /**
-     * @author yukunlee
-     * @Description recover-binary-search-tree
-     * @date 2018年11月9日
-     * @param root
-     */
-    public void recoverTree(TreeNode root) {
-        
-    }
 
-	
 }
