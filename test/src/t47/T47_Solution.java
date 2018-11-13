d84f74353b95a0786626893b165e60caafb641fe
RichearthLee <214503210@qq.com>
2018-11-13 15:11:19 +0800

    update

--- test/src/t47/T47_Solution.java
+++ test/src/t47/T47_Solution.java
@@ -58,5 +58,27 @@ public class T47_Solution {
     	
     	return true;
     }
+    
+    //DP动态规划
+    public boolean isInterleave_v3(String s1, String s2, String s3) { 
+    	int len1 = s1.length() , len2 = s2.length() , len3 = s3.length();
+    	char[] ch1 = s1.toCharArray() , ch2 = s2.toCharArray() , ch3 = s3.toCharArray();
+    	boolean[][] dp= new boolean[len1+1][len2+1]; 
+    	
+    	if(len1 + len2 != len3) return false;
+    	dp[0][0] = true;
+    	for(int i = 1 ; i <= len1 ; ++i) {
+    		dp[i][0] = dp[i-1][0] && ch1[i-1] == ch3[i-1];
+    	}
+    	for(int i = 1 ; i <= len2 ; ++i) {
+    		dp[0][i] = dp[0][i-1] && ch2[i-1] == ch3[i-1];
+    	}
+    	for(int i = 1 ; i <= len1 ; ++i) {
+    		for(int j = 1 ; j <= len2 ; ++j) {
+    			dp[i][j] = (dp[i-1][j] && ch1[i-1] == ch3[i+j-1])||(dp[i][j-1] && ch2[j-1] == ch3[i+j-1]);
+    		}
+    	}
+    	return dp[len1][len2];
+    }
 
 }
