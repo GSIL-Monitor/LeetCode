d7da4bde6a1fdda0ab0fec754c064562047489cb
RichearthLee <214503210@qq.com>
2018-11-20 15:39:41 +0800

    update

--- test/src/t49/T49_Solution.java
+++ test/src/t49/T49_Solution.java
@@ -145,6 +145,44 @@ public class T49_Solution {
         }
         return arr;
     }
+	
+	
+    /**
+     * @author yukunlee
+     * @Description scramble-string
+     * @date 2018年11月20日
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean isScramble(String s1, String s2) {
+        if(s1.equals(s2))
+        	return true;
+        if(s1.length() != s2.length()) 
+        	return false;
+        int len = s1.length();
+        int[] letter = new int[26];
+        for(int i = 0 ; i < len ; ++i) {
+        	letter[s1.charAt(i) - 'a']++;
+        	letter[s2.charAt(i) - 'a']--;
+        }
+        for(int i = 0 ; i < 26 ; ++i) {
+        	if(letter[i] != 0)
+        		return false;
+        }
+        
+        for(int i = 1 ; i < len ; ++i) {
+        	if(isScramble(s1.substring(0, i), s2.substring(0,i)) 
+        			&& isScramble(s1.substring(i), s2.substring(i))) {
+        		return true;
+        	}
+        	if(isScramble(s1.substring(0, i), s2.substring(len - i))
+        			&& isScramble(s1.substring(i), s2.substring(0, len - i))) {
+        		return true;
+        	}
+        }
+    	return false;
+    }
 
 	
 		
