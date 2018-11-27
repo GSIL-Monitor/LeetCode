010e558c7c176fcdd54aecb1e41f61974cf9d2e0
RichearthLee <214503210@qq.com>
2018-11-27 10:43:56 +0800

    update

--- leetcode/src/t50/T50_Solution.java
+++ leetcode/src/t50/T50_Solution.java
@@ -35,8 +35,8 @@ public class T50_Solution {
 		 */
 		while (tail < slen) {
 			ch = S.charAt(tail++);
-			if (map.containsKey(ch) ) {
-				if(map.get(ch) > 0) {
+			if (map.containsKey(ch)) {
+				if (map.get(ch) > 0) {
 					count--;
 				}
 				map.put(ch, map.get(ch) - 1);
@@ -56,30 +56,72 @@ public class T50_Solution {
 		}
 		return d == Integer.MAX_VALUE ? "" : S.substring(start, start + d);
 	}
+
+	/**
+	 * @author yukunlee
+	 * @Description sort-colors
+	 * @date 2018年11月26日
+	 * @param A
+	 */
+	public void sortColors(int[] A) {
+		int one = 0, two = A.length - 1;
+
+		for (int i = 0; i <= two; ++i) {
+			if (A[i] < 1) {
+				A[i] = A[one];
+				A[one] = 0;
+				++one;
+			} else if (A[i] > 1) {
+				A[i] = A[two];
+				A[two] = 2;
+				--two;
+				--i;
+			}
+		}
+	}
+
+	/**
+	 * @author yukunlee
+	 * @Description search-a-2d-matrix
+	 * @date 2018年11月27日
+	 * @param matrix
+	 * @param target
+	 * @return
+	 */
+	public boolean searchMatrix_v1(int[][] matrix, int target) {
+		for (int i = 0; i < matrix.length; ++i) {
+			if (matrix[i][0] > target) {
+				if (i - 1 < 0)
+					return false;
+				for (int j = 0; j < matrix[0].length; ++j) {
+					if (matrix[i - 1][j] == target)
+						return true;
+				}
+				break;
+			}
+		}
+		if (matrix[matrix.length-1][matrix[0].length-1] < target) {
+			for (int j = 0; j < matrix[0].length; ++j) {
+				if (matrix[matrix.length][j] == target)
+					return true;
+			}
+		}
+		return false;
+	}
 	
+	public boolean searchMatrix(int[][] matrix, int target) {
+		int row = 0 ,col = matrix[0].length-1;
+		while(row < matrix.length && col >= 0) {
+			if(target == matrix[row][col]) return true;
+			else if(target < matrix[row][col]) {
+				--col;
+			}
+			else if(target > matrix[row][col]) {
+				++row;
+			}
+		}
+		return false;
+	}
+
 	
-    /**
-     * @author yukunlee
-     * @Description sort-colors
-     * @date 2018年11月26日
-     * @param A
-     */
-    public void sortColors(int[] A) {
-        int one = 0 , two = A.length-1;
-        
-        for(int i = 0 ; i <= two ; ++i) {
-        	if(A[i] < 1) {
-        		A[i] = A[one];
-        		A[one] = 0;
-        		++one;
-        	}else if(A[i] > 1) {
-           		A[i] = A[two];
-        		A[two] = 2;
-        		--two;
-        		--i;
-        	}
-        }
-    }
-    
-    
 }
