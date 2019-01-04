c2be1ad17129d3f949ddb2271be936a3681892d3
Yukun_Lee <214503210@qq.com>
2019-01-04 15:34:48 +0800

    update

--- LeetCode/Python/AdvancedPythonScheduler/scheduler.py
+++ LeetCode/Python/AdvancedPythonScheduler/scheduler.py
@@ -1,6 +1,8 @@
+import time
+import os
 from datetime import datetime
 from datetime import date
-import time
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
@@ -11,6 +13,14 @@ def job_func():
     print (time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
     #print(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3])
 
+def func_1():
+    while(True):
+        path = "E:/test/"
+        #os.mkdir(path+"test.txt")
+        file = open(path+"test.txt",'w')
+        file.write('1')
+        file.close
+
 scheduler = BackgroundScheduler()
 # 在 2017-12-13 时刻运行一次 job_func 方法
 #scheduler .add_job(job_func, 'date', run_date=date(2017, 12, 13), args=['text'])
@@ -19,6 +29,8 @@ scheduler = BackgroundScheduler()
 # 在 2017-12-13 14:00:01 时刻运行一次 job_func 方法
 #scheduler .add_job(job_func, 'date', run_date='2019-1-3 18:24:01', args=['text'])
 
-# 每隔两分钟执行一次 job_func 方法
-scheduler .add_job(job_func, 'interval', seconds=5)
+scheduler.add_job(job_func, 'interval', seconds=5,id='1')
+scheduler.add_job(func_1, 'interval', seconds=5,id='2')
+
 scheduler.start()
+print("end")
